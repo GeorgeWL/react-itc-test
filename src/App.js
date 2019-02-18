0f0d6053d812ccb2c82406b7b281462a94ec454e
George Remote <georgelangham1990@gmail.com>
2019-02-18 15:50:36 +0000

    header, title and footer styles

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   // init state
   state = {
     items: TEST_DATA,
-  } 
+  }
   // I imagine in a real app would handle this with an
   // external data store such as mysql or graphQL, 
   // probably with redux or similar to control the data
@@ -25,20 +25,44 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>
-            Vacancies
-          </h1>
-          To get a feel for what it's like to work at ITC
+          <h1 className="logo"><a href="/">ITC</a></h1>
+          <ul className="main-nav">
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Who We Are</a></li>
+            <li><a href="/">What We Do</a></li>
+            <li className={'active'}><a href="/">Vacancies</a></li>
+            <li><a href="/">News</a></li>
+            <li><a href="/">Contact Us</a></li>
+          </ul>
         </header>
         <main>
+          <div
+            className={'header'}
+          >
+            <h1
+              className={'title'}
+            >
+              Vacancies
+            </h1>
+            To get a feel what it's like to work or ITC, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </div>
           <VacancyForm
             onSubmit={this.onSubmit}
           />
-          <VacancyList 
+          <VacancyList
             items={items}
           />
         </main>
-        <footer>
+        <footer className='App-footer'>
+          <h1 className="logo"><a href="/">ITC</a></h1>
+          <ul className="footer-nav">
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Who We Are</a></li>
+            <li><a href="/">What We Do</a></li>
+            <li><a href="/">Vacancies</a></li>
+            <li><a href="/">News</a></li>
+            <li><a href="/">Contact Us</a></li>
+          </ul>
         </footer>
       </div>
     );
